c747417f87bd8b6a4ec5c9a7d01b4f1c6175a045
Nazneen Samu Maricar <nazneenmaricar@Nazs-MacBook-Pro.local>
2017-05-18 15:47:01 -0400

    got the journals

--- src/app/service/journal-data.service.ts
+++ src/app/service/journal-data.service.ts
@@ -8,22 +8,29 @@ import { JournalList } from '../model/journal.list';
 
 @Injectable ()
 export class JournalDataService {
+
   journalsUrl = "http://portal.helloitscody.com/inhabitent/api/get/94a08da1fecbb6e8b46990538c7b50b2/";
 
+  private postJournalUrl = "http://portal.helloitscody.com/inhabitent/api/post/94a08da1fecbb6e8b46990538c7b50b2";
 
-  constructor (private http: Http) { }
-  getJournals(): Promise<Journal[]> {
-    console.log ('in getJournals() method of JournalDataService');
+  constructor (private http: Http) { };
 
-    let newPromise: any =
-    this.http.get(this.journalsUrl)
-    .toPromise ()
-    .then (resp => { return resp.json();} )
-    .catch( err=> {console.log(err)} );
+  getJournals(): any {
+    // console.log ("JournalDataService");
 
-    return newPromise;
+    const getTheData = this.http.get(this.journalsUrl);
+    const newPromise: any = getTheData.toPromise();
+    const successFn: Function = resp => { return resp.json(); };
+    const failureFn: Function = err => { console.log(err) };
+    const resolvedPromise: any = Promise.resolve(newPromise.then(successFn).catch(failureFn));
+    return resolvedPromise;
   }
-  handleError(error) {
-    console.log(error);
+
+  postJournal(): any {
+    console.log("postJournal");
+    // this.postJournalUrl= this.journalPostUrl + "params=" + params;
+
+
+
   }
 }
